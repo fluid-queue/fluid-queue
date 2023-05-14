ddf7982e59e038f3f8f9cd244edb03ef1ae0f99d
Nya <7364785+liquidnya@users.noreply.github.com>
2023-05-14 12:05:06 +0200

    Add build version Check build version on startup

--- src/banner.ts
+++ src/banner.ts
@@ -1,3 +1,5 @@
+import { version, buildVersion, buildTag } from "./version.js";
+
 const bannerTemplate = ` ___  _          _      _
 /  _|| |        |_|    | | ###################################
 | |_ | |  _   _  _  ___| |     _____  _   _  ____  _   _  ____
@@ -8,34 +10,44 @@ const bannerTemplate = ` ___  _          _      _
  ~ ><))°> o° .  ~ ><))°> ~   o°  .|_|  ~ ><))°>  °o ><))°> o( )°
 `;
 
-const version = () => {
-  // Return an empty string if there's no environment
-  if (!process || !process.env) {
-    return "";
+const displayVersion = () => {
+  const buildV = buildVersion();
+  if (buildV == null) {
+    return version();
   }
+  const buildT = buildTag();
+  if (buildT == null) {
+    return buildV;
+  }
+  return `${buildV} (${buildT})`;
+};
 
-  // Use the Docker variables if they exist and this is the develop tag
-  // (or the "this" tag which seems to be used for automated tests???)
-  if (
-    (process.env.DOCKER_TAG == "this" || process.env.DOCKER_TAG == "develop") &&
-    process.env.SOURCE_COMMIT
-  ) {
-    return process.env.DOCKER_TAG + "-" + process.env.SOURCE_COMMIT.slice(0, 8);
+const checkVersion = (): boolean | null => {
+  const buildV = buildVersion();
+  if (buildV == null) {
+    // can not tell if versions match
+    return null;
   }
-
-  // Use the NPM version if it's available
-  if (process.env.npm_package_version != null) {
-    return "version " + process.env.npm_package_version;
+  const v = version();
+  if (v == "") {
+    // can not tell if versions match
+    return null;
   }
-  return "";
+  return buildV == v;
 };
 
 const printBanner = () => {
   try {
     const banner = bannerTemplate.replace(/#+/g, (characters) =>
-      version().padStart(characters.length, " ")
+      displayVersion().padStart(characters.length, " ")
     );
     console.log(banner);
+    if (checkVersion() === false) {
+      console.warn(
+        `Warning: Running a different version of the queue (${buildVersion()} instead of ${version()})`
+      );
+      console.warn("(Use `npm run build` to build the current version)\n");
+    }
   } catch (e) {
     // ignore error
   }
