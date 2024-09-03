a7ad20dfa1af30af5ce7bc2229dcf28c8bd13796
demize <demize@unstable.systems>
2024-09-03 13:47:51 -0400

    Rework database schema (removing existing migrations)

--- drizzle/migrations/0001_queue_current_check_constraint.sql
+++
@@ -1,9 +0,0 @@
--- Custom SQL migration file, put you code below! --
-
--- Check constraint on current
-
-DO $$ BEGIN
- ALTER TABLE "queue" ADD CONSTRAINT "queue_current_check" CHECK("current");
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,21 @@
 import {
   pgTable,
+  decimal,
   uuid,
   varchar,
   timestamp,
   boolean,
   interval,
-  integer,
   json,
 } from "drizzle-orm/pg-core";
 
-export const ChatterTable = pgTable("chatters", {
+import { relations } from "drizzle-orm";
+
+// queue.json: waiting
+// keep track of people who have submitted levels, and how long they've waited
+// this also functions as our user table; rather than fk into a user table from
+// this and from the queue, we fk into this from the queue
+export const WaitingTable = pgTable("waiting", {
   id: varchar("id").primaryKey(),
   name: varchar("name").notNull(),
   displayName: varchar("display_name").notNull(),
@@ -18,6 +24,8 @@ export const ChatterTable = pgTable("chatters", {
   weight: interval("weight").notNull(),
 });
 
+// queue.json: entries[current|queue] (no submitter data)
+// Keep track of levels, for the queue and for other purposes
 export const LevelTable = pgTable("levels", {
   id: uuid("id").primaryKey(),
   type: varchar("type").notNull(),
@@ -25,20 +33,14 @@ export const LevelTable = pgTable("levels", {
   extension_data: json("extension_data"),
 });
 
-export const LevelStatsTable = pgTable("level_statistics", {
-  id: uuid("id")
-    .references(() => LevelTable.id)
-    .primaryKey(),
-  first_submitted: timestamp("submitted").notNull(),
-  play_count: integer("play_count").notNull(),
-  play_time: interval("total_time", { precision: 0 }),
-});
-
+// queue.json: entries[current] and entries[queue]
+// current has a unique and check constraint that means it can only be either
+// true or null, and only a single row can be true
 export const QueueTable = pgTable("queue", {
   id: uuid("id").primaryKey(),
   submitted: timestamp("submitted").notNull(),
   submitter: varchar("submitter")
-    .references(() => ChatterTable.id)
+    .references(() => WaitingTable.id)
     .notNull(),
   level: uuid("level")
     .references(() => LevelTable.id)
@@ -46,3 +48,26 @@ export const QueueTable = pgTable("queue", {
   current: boolean("current").unique(), // check constraint is unimplemented in drizzle-orm, but we created it ourselves in a custom migration
   weight: interval("weight").notNull(),
 });
+
+// queue.json: extensions
+// extension data will be stored in the database, as JSON
+export const ExtensionDataTable = pgTable("extension_data", {
+  id: uuid("id").primaryKey(),
+  version: decimal("version"),
+  data: json("data"),
+});
+
+// *********
+// relations
+// *********
+
+export const QueueRelations = relations(QueueTable, ({ one }) => ({
+  waiting: one(WaitingTable, {
+    fields: [QueueTable.submitter],
+    references: [WaitingTable.id],
+  }),
+  level: one(LevelTable, {
+    fields: [QueueTable.level],
+    references: [LevelTable.id],
+  }),
+}));
